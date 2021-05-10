0d18ef6a0a4dcc93f8eefed5d04b3f78d6369213
Your Name <you@example.com>
2021-05-11 00:03:12 +0800

    新增testliu

---
+++ src/main/java/testliu.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * @author zhanchang
+ * @version 1.0
+ * @date 2021/4/27 23:08
+ */
+public class testliu {
+public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    System.out.println("请问你的名字是？");
+    String str = in.nextLine();
+    System.out.println(str+ "，你好。");
+}
+}
